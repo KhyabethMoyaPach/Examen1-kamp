36cc2fc818b7723168bca3d7cc45c3986bcb99f9
KhyabethMoyaPach <khyabeth.moya@uab.edu.bo>
2025-08-03 17:35:44 -0400

    crear seeder de producto y configurar databasaseseeder

---
+++ laravelBackend/database/seeders/ProductoSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProductoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        DB::table('producto')->insert([
+            'nombre' => 'Nike Air Jordan 5 Retro',
+            'precio' => 216.00,
+            'id_marca' => 3,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('producto')->insert([
+            'nombre' => 'Supreme Hand Tied Beanie',
+            'precio' => 49.99,
+            'id_marca' => 2,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('producto')->insert([
+            'nombre' => 'Pantalones de lino de talle alto',
+            'precio' => 111.00,
+            'id_marca' => 1,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+}
