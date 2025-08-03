3f26da4a02c4446c125c43c28062a90bd742c9f9
KhyabethMoyaPach <khyabeth.moya@uab.edu.bo>
2025-08-03 18:46:18 -0400

    configurar rutas de api

---
+++ laravelBackend/routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MarcaController;
+use App\Http\Controllers\ProductoController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::apiResource('marca',MarcaController::class);
+Route::apiResource('producto',ProductoController::class);
