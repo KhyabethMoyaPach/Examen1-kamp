f1f57bcd832d78bc3a00668697955d6e9715f005
KhyabethMoyaPach <khyabeth.moya@uab.edu.bo>
2025-08-03 20:32:50 -0400

    crear controlador de auth

--- laravelBackend/routes/api.php
+++ laravelBackend/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MarcaController;
 use App\Http\Controllers\ProductoController;
 
@@ -11,3 +12,7 @@ Route::get('/user', function (Request $request) {
 
 Route::apiResource('marcas',MarcaController::class);
 Route::apiResource('productos',ProductoController::class);
+
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::post('/login', [AuthController::class, 'login']);
